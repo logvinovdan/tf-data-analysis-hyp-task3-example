33b14a2ef147db67830f17f6f294061d708db5c3
logvinovdan <76569554+logvinovdan@users.noreply.github.com>
2023-04-13 22:53:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import ttest_ind
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 121482204 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(control_sample, test_sample) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.09
+    _, pval = ttest_ind(control_sample, test_sample, equal_var=False, alternative="two-sided")
+    return pval < alpha # Ваш ответ, True или False
